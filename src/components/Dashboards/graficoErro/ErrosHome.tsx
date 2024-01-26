936eed1eec09f110b425fad9fcaac77e23a0396b
kessiamilena <kessia.milena124@yahoo.com>
2024-01-26 16:34:42 -0300

    alterações errosHome

--- src/components/Dashboards/graficoErro/ErrosHome.tsx
+++ src/components/Dashboards/graficoErro/ErrosHome.tsx
@@ -5,8 +5,10 @@ import Chart from "react-apexcharts";
 import "../graficoErro/style.css"
 
 // type ContagemAno = {
-//     ano : number;
-//     contagem : number
+//     erroList: [
+//       ano : number,
+//       contagem : number
+//     ]
 // }
 
 // export const Ano = createContext({} as ContagemAno );
@@ -20,8 +22,8 @@ function ErrosHome(props) {
       id: "basic-bar"
     },
     xaxis: {
-      categories: [10,20,30]
-      // categories: props.anoList
+      // categories: [10,20,30]
+      categories: props.anoList
       // categories: [props.data_erro]
     }
   };
